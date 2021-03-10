971b0ba0cb82239e0a530ef60930f17758099a57
Sami <sami.seisalo@gmail.com>
2021-03-10 18:12:41 +0200

    frontend added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', (req, res) => { return JSON.stringify(req.body)})
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
